31a6eaacc48d163f117e4ff18be386ec59d082c6
Joe Healy <jhealy@microsoft.com>
2017-11-16 13:47:45 -0600

    Zxing project added

---
+++ ZXingPlaySLN/ZXingPlay/ZXingPlay/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ZXingPlay
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Scan_Clicked(object sender, EventArgs e)
+        {
+            // Label_ScanResult.Text = $"{DateTime.UtcNow.ToLocalTime().ToString()}";
+            await DoScan();
+        }
+
+        private async Task DoScan()
+        {
+            var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+            var result = await scanner.Scan();
+
+            if (result != null)
+            {
+                Label_ScanResult.Text = result.Text;
+            }
+            else
+            {
+                Label_ScanResult.Text = "no results returned";
+            }
+        }
+    }
+}
